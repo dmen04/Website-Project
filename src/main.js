eaba7c855f3201d27d2a6849a0974b4ab8c69d3e
Infobaz2K <daviseliassmencis@gmail.com>
2025-11-02 15:07:35 +0200

    Add navbar logo animation

--- src/main.js
+++ src/main.js
@@ -5,7 +5,29 @@ async function main() {
     const roster = await getPlayers();
     const team = await getTeamInfo();
 }
-
 main();
 
 
+const topHeader = document.getElementById("topHeader");
+const logo = document.querySelector("#logo img");
+const logoContainer = document.getElementById("logo");
+const mainHeader = document.getElementById("mainHeader");
+
+function handleScroll() {
+    const scrollY = window.scrollY;
+
+    if(scrollY > 80) {
+        topHeader.classList.add("-translate-y-full");
+        logo.classList.replace("w-40", "w-16");
+        logo.classList.replace("h-40", "h-16");
+        logoContainer.classList.replace("top-12", "top-1/2");
+    } else {
+        topHeader.classList.remove("-translate-y-full");
+        logo.classList.replace("w-16", "w-40");
+        logo.classList.replace("h-16", "h-40");
+        logoContainer.classList.replace("top-1/2", "top-12");
+    }
+}
+
+window.addEventListener("scroll", handleScroll);
+handleScroll();
